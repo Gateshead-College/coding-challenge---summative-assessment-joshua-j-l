4a0a2f55a9925af2b8f79b93a5958480166967ba
Lawlor <joshua.lawlor@accenture.com>
2021-12-14 10:50:53 +0000

    Task 2 plan complete, closes #4

--- src/summative_assessment/ProjectPlan.java
+++ src/summative_assessment/ProjectPlan.java
@@ -3,8 +3,19 @@ package summative_assessment;
 public class ProjectPlan {
 }
 
+
+
+//Task 2
+// create a data model for stockItem
+// amend the population data for the stockList
+// amend the output when displaying stock
+
+
+
 // Task 1
 // Create stock ArrayList
 // Populate ArrayList with stock items
 // Get main menu. Option 1 - show stock
 // print ArrayList
+
+
