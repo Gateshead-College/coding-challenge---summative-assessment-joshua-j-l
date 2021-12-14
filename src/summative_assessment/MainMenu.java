ebe661714910fc24c0106bcf29bd3b00f8288e30
Lawlor <joshua.lawlor@accenture.com>
2021-12-14 10:34:24 +0000

    Main menu with show stock option completed, closes #3

--- src/summative_assessment/MainMenu.java
+++ src/summative_assessment/MainMenu.java
@@ -1,14 +1,18 @@
 package summative_assessment;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainMenu {
 
     ArrayList<String> stockList = new ArrayList();
+    Scanner scn = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.populateList();
+        MainMenu startProgram = new MainMenu();
+        startProgram.populateList();
+        startProgram.mainMenu();
 
     }
 
@@ -22,4 +26,46 @@ public class MainMenu {
         stockList.add("Tanzanite");
     }
 
+    private void mainMenu() {
+        System.out.println("welcome to Wilbur's Emporium!");
+        System.out.println("What would you like to do today?");
+        System.out.println("1 - View the stock list.");
+        System.out.println("2 - Leave the Emporium.");
+        Integer mainMenuChoice = Integer.valueOf(scn.nextLine());
+        switch (mainMenuChoice) {
+            case 1:
+                viewStockList();
+                break;
+            case 2:
+                System.out.println("Have a nice day!");
+                break;
+            default:
+                return;
+
+        }
+
+    }
+
+    private void viewStockList() {
+        for (int i = 0; i < stockList.size(); i++) {
+            System.out.println(stockList.get(i));
+            System.out.println();
+        }
+        mainMenu();
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
