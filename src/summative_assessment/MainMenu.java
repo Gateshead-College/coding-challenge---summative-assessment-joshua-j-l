f7a34cefb1a3d4a462b91613c9db8fdcbd2a8a97
Lawlor <joshua.lawlor@accenture.com>
2021-12-14 10:11:00 +0000

    ArrayList created and populated, closes #2

--- src/summative_assessment/MainMenu.java
+++ src/summative_assessment/MainMenu.java
@@ -1,4 +1,25 @@
 package summative_assessment;
 
+import java.util.ArrayList;
+
 public class MainMenu {
+
+    ArrayList<String> stockList = new ArrayList();
+    public static void main(String[] args) {
+
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.populateList();
+
+    }
+
+    private void populateList() {
+        stockList.add("Diamond");
+        stockList.add("Ruby");
+        stockList.add("Amethyst");
+        stockList.add("Sapphire");
+        stockList.add("Emerald");
+        stockList.add("Opal");
+        stockList.add("Tanzanite");
+    }
+
 }
--- src/summative_assessment/ProjectPlan.java
+++ src/summative_assessment/ProjectPlan.java
@@ -4,7 +4,7 @@ public class ProjectPlan {
 }
 
 // Task 1
-// Create stock ListArray
-// Populate ListArray with stock items
+// Create stock ArrayList
+// Populate ArrayList with stock items
 // Get main menu. Option 1 - show stock
-// print ListArray
+// print ArrayList
